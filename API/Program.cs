140b94de288afd901afd71133d56407e6bffee40
Giusxepe <betosft_12@hotmail.com>
2023-06-28 15:53:03 -0600

    End of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,21 @@
 using API.Extensions;
 using API.Middleware;
-using Application.Activities;
-using Application.Core;
-using MediatR;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => {
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddAplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -28,6 +33,8 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
+app.UseAuthentication();
+
 
 app.MapControllers();
 
@@ -37,7 +44,8 @@ try
 {
     var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
